6ca1c61a6a06910170cc63d991de8a1f3afb30bb
KoJaem <woalswhwh@naver.com>
2024-01-07 16:39:25 +0900

    feat: TextBoxComponent (text 내용 출력) 구현

---
+++ lib/constants/customColor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color darkGray = Color(0xFF777777);
+  static const Color brightGray = Color(0xffeaeaea);
+  static const Color darkBlueGray = Color(0xff869bb3);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xFF000000);
+}
--- lib/project_view_component.dart
+++ lib/project_view_component.dart
@@ -5,10 +5,12 @@ import 'package:flame/components.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 import 'package:jenny/jenny.dart';
+import 'package:jenny_study/constants/customColor.dart';
 import 'package:jenny_study/main.dart';
 
 class ProjectViewComponent extends PositionComponent
     with DialogueView, HasGameRef<JennyGame> {
+  late final TextBoxComponent mainDialogueTextComponent;
   final background = SpriteComponent();
   final girl1 = SpriteComponent();
   final girl2 = SpriteComponent();
@@ -17,6 +19,13 @@ class ProjectViewComponent extends PositionComponent
 
   @override
   FutureOr<void> onLoad() {
+    final dialogPaint = TextPaint(
+        style: const TextStyle(
+      color: CustomColor.white,
+      backgroundColor: CustomColor.darkBlueGray,
+      fontSize: 20,
+      height: 1.4,
+    ));
     background
       ..sprite = gameRef.backgroundSprite
       ..size = gameRef.size;
@@ -41,7 +50,24 @@ class ProjectViewComponent extends PositionComponent
           }
         });
 
-    addAll([background, girl1, girl2, forwardButtonComponent]);
+    mainDialogueTextComponent = TextBoxComponent(
+      text: '',
+      textRenderer: dialogPaint,
+      position: Vector2(
+        gameRef.size.x * .05,
+        gameRef.size.y * .65,
+      ),
+      boxConfig: TextBoxConfig(
+        maxWidth: gameRef.size.x * .9,
+      ),
+    );
+    addAll([
+      background,
+      girl1,
+      girl2,
+      forwardButtonComponent,
+      mainDialogueTextComponent,
+    ]);
     return super.onLoad();
   }
 
@@ -55,9 +81,9 @@ class ProjectViewComponent extends PositionComponent
   Future<void> _advance(DialogueLine line) async {
     var characterName = line.character?.name;
     var lineText = line.text;
-    // var dialogueLineText = '$characterName ${line.text}';
     var dialogueText =
         characterName != null ? '$characterName: $lineText' : lineText;
+    mainDialogueTextComponent.text = dialogueText;
     debugPrint('debug: $dialogueText');
     return _forwardCompleter.future;
   }
